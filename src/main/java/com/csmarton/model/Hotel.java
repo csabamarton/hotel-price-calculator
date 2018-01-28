a5170d33c2e1a97bc67f10061460b9d9fdcba0e4
Csaba Marton <martoncsab@gmail.com>
2018-01-28 13:07:28 +0100

    first bunch of settings: gitignore, build.gradle, src

---
+++ src/main/java/com/csmarton/model/Hotel.java
@@ -0,0 +1,15 @@
+package com.csmarton.model;
+
+import java.util.Map;
+
+public class Hotel {
+    private String hotelId;
+    private String name;
+    private Map address;
+    private Contact contact;
+    private String phone;
+    //Todo make this to enum
+    private String hotelType;
+
+
+}
